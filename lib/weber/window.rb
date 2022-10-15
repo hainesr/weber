e7035767c4b2dfcb5e8ad3451117dd74c736717a
Robert Haines <rhaines@manchester.ac.uk>
2022-10-15 18:44:56 +0100

    Implement rudimentary scrolling.

--- lib/weber/window.rb
+++ lib/weber/window.rb
@@ -12,20 +12,31 @@ module WeBER
   class Window
     WIDTH  = 800
     HEIGHT = 600
+    SCROLL_STEP = 100
 
     def initialize
       root = TkRoot.new
+      root.bind('Down') { scroll_down }
       @canvas = TkCanvas.new(root) do
         width(WIDTH)
         height(HEIGHT)
       end
       @canvas.pack
+      @scroll = 0
     end
 
-    def draw(display_list)
-      display_list.each do |x, y, c|
-        @canvas.create('text', x, y, text: c)
+    def draw(display_list = @display_list)
+      @display_list ||= display_list
+      @canvas.delete('all')
+
+      @display_list.each do |x, y, c|
+        @canvas.create('text', x, y - @scroll, text: c)
       end
+    end
+
+    def scroll_down
+      @scroll += SCROLL_STEP
+      draw
     end
 
     def width
