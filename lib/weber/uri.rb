c8ec5ddfca5e8e924f3ca129bf3a1c87c5d60df3
Robert Haines <rhaines@manchester.ac.uk>
2022-10-15 11:22:23 +0100

    Update URI to support the data scheme.

--- lib/weber/uri.rb
+++ lib/weber/uri.rb
@@ -8,14 +8,18 @@
 
 module WeBER
   class URI
-    attr_reader :host, :path, :port, :scheme
+    attr_reader :data, :host, :media_type, :path, :port, :scheme
 
     def initialize(uri)
-      @scheme, rest = uri.split('://')
-      host, path = rest.split('/', 2)
-      @host, port = host.split(':')
-      @path = "/#{path}"
-      @port = port&.to_i
+      @scheme, rest = uri.split(':', 2)
+      if @scheme == 'data'
+        @media_type, @data = rest.split(',', 2)
+      else
+        host, path = rest.delete_prefix('//').split('/', 2)
+        @host, port = host.split(':')
+        @path = "/#{path}"
+        @port = port&.to_i
+      end
     end
   end
 end
--- test/uri_test.rb
+++ test/uri_test.rb
@@ -12,39 +12,61 @@ require 'weber/uri'
 
 class URITest < Minitest::Test
   def setup
+    @data  = 'data'
     @file  = 'file'
     @https = 'https'
     @http  = 'http'
     @host  = 'example.org'
+    @media = 'text/html'
     @port  = 8080
     @path  = '/this/is/a/path'
+    @text  = 'Hello, world!'
 
+    @data_uri  = WeBER::URI.new("#{@data}:#{@media},#{@text}")
     @file_uri  = WeBER::URI.new("#{@file}://#{@path}")
     @https_uri = WeBER::URI.new("#{@https}://#{@host}#{@path}")
     @http_uri  = WeBER::URI.new("#{@http}://#{@host}:#{@port}#{@path}")
   end
 
   def test_scheme
+    assert_equal(@data, @data_uri.scheme)
     assert_equal(@file, @file_uri.scheme)
     assert_equal(@https, @https_uri.scheme)
     assert_equal(@http, @http_uri.scheme)
   end
 
   def test_host
+    assert_nil(@data_uri.host)
     assert_nil(@file_uri.host)
     assert_equal(@host, @https_uri.host)
     assert_equal(@host, @http_uri.host)
   end
 
   def test_path
+    assert_nil(@data_uri.path)
     assert_equal(@path, @file_uri.path)
     assert_equal(@path, @https_uri.path)
     assert_equal(@path, @http_uri.path)
   end
 
   def test_port
+    assert_nil(@data_uri.port)
     assert_nil(@file_uri.port)
     assert_nil(@https_uri.port)
     assert_equal(@port, @http_uri.port)
+  end
+
+  def test_data
+    assert_equal(@text, @data_uri.data)
+    assert_nil(@file_uri.data)
+    assert_nil(@https_uri.data)
+    assert_nil(@http_uri.data)
+  end
+
+  def test_media_type
+    assert_equal(@media, @data_uri.media_type)
+    assert_nil(@file_uri.media_type)
+    assert_nil(@https_uri.media_type)
+    assert_nil(@http_uri.media_type)
   end
 end
